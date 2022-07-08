4f8274294353b96f78dff9358747e29616785c0b
Chris <git@smithc.dev>
2022-07-08 12:58:48 -0400

    intial app with error handling

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
+//const storeRouter = require('./routes/storeRouter')
+//const userRouter = require('./routes/userRouter')
+//const productRouter = require('./routes/productRouter')
+
+const app = express();
+
+// 1) Global Middlewares
+// Development Logging Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// 2) Routes
+//app.use('/api/v1/stores', storeRouter);
+//users, userRouter
+//products, productRouter
+
+// 3) Error Handling Middlewares
+// Throw new error for any undefined routes
+app.get('*', (req, res, next) => {
+  next(new AppError(404, `${req.originalUrl} not found`));
+});
+app.use(globalErrorHandler);
+
+module.exports = app;
