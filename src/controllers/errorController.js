72e948236f44dab0ae5e9bd3e3091b5cc19e82dc
Chris Smith <git@smithc.dev>
2022-07-13 16:01:24 +0000

    added validator duplicateFields and castError functions to the production error handler

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,3 +1,28 @@
+const AppError = require('../utils/appError');
+
+// Error when using a bad _id
+const castErrorHandler = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(400, message);
+};
+
+// Error when submiting invalid schema values
+const validatorErrorHandler = (err) => {
+  const errors = Object.values(err.errors).map((item) => {
+    return `${item.path}: ${item.message}`;
+  });
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(400, message);
+};
+
+// Error when submitting the same value marked as unique by the schema
+const duplicateFieldsHandler = (err) => {
+  // User.email is currently the only unique value
+  const value = err.keyValue.email;
+  const message = `${value} already exists. Please use another value.`;
+  return new AppError(400, message);
+};
+
 const sendErrorProd = (err, res) => {
   // Operational, trusted error: send message to client
   if (err.isOperational) {
@@ -29,7 +54,11 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
   if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = JSON.parse(JSON.stringify(err));
+    if (error.name === 'CastError') error = castErrorHandler(error);
+    if (error.name === 'ValidationError') error = validatorErrorHandler(error);
+    if (error.code === 11000) error = duplicateFieldsHandler(error);
+    sendErrorProd(error, res);
   } else if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   }
