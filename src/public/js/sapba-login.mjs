12d17261b62309ee25245ee0df1c903fff4600f0
Chris Smith <git@smithc.dev>
2023-01-19 10:59:43 -0500

    add demo login buttons

--- src/public/js/sapba-login.mjs
+++ src/public/js/sapba-login.mjs
@@ -2,6 +2,9 @@ import { login, forgotPassword } from './modules/auth';
 
 const loginForm = document.querySelector('.form-login');
 const forgotPass = document.querySelector('.forgotPassword-form');
+const adminLogin = document.getElementById('admin-login');
+const managerLogin = document.getElementById('manager-login');
+const employeeLogin = document.getElementById('employee-login');
 
 if (loginForm) {
   loginForm.addEventListener('submit', (e) => {
@@ -17,5 +20,24 @@ if (forgotPass) {
     e.preventDefault();
     const email = document.getElementById('email').value;
     forgotPassword(email);
+  });
+}
+
+if (adminLogin) {
+  adminLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    login('admin@sapbashop.com', 'pass1234');
+  });
+}
+if (managerLogin) {
+  managerLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    login('manager@sapbashop.com', 'pass1234');
+  });
+}
+if (employeeLogin) {
+  employeeLogin.addEventListener('click', (e) => {
+    e.preventDefault();
+    login('employee@sapbashop.com', 'pass1234');
   });
 }
